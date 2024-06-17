34554391e1b6c423d541ea226576d4e3a6900d66
michbrew <michbrew1978@gmail.com>
2024-06-17 15:57:10 +0900

    formatting revisions with the output, blank lines and tabs

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-const jokesArray = require("./jokes");
 
-const introString = "Here is the Dad Joke of the Day, I hope that you enjoy it as much as I did!"
-
-const closingString = "Until next time, keep smiling!"
-
-//console.log(jokesArray.length);
 
 const bigString = () => {
+    
+    const jokesArray = require("./jokes");
+
+    const introString = "Here is the Dad Joke of the Day, I hope that you enjoy it as much as I did!";
+
+    const closingString = "Until next time, keep smiling!";
+
     const randomNumber = Math.floor(Math.random()*150);
     
-    return `\n\n${introString}\n\n"${jokesArray[randomNumber]}"\n\n${closingString}\n\n`;
+    return `\n\n\t${introString}\n\n\n\t\t"${jokesArray[randomNumber]}"\n\n\n\t${closingString}\n\n`;
 }
 
 console.log(bigString());
